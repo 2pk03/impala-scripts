c6ed7c7815097e25f3eaae5ca455c48e3b6edad6
alexander <alexander@hadoop1.(none)>
2013-05-11 09:12:01 +0200

    - minor tweaks - add new Impala query - add HBase get

--- zipcode-hbase.sh
+++ zipcode-hbase.sh
@@ -4,13 +4,13 @@ HBASE_TABLE=zipcode_hive
 IMPALA_HOST=`hostname`:21000
 IMPALA_OPTS=-k
 
+clear
+
 unzip -u median_income_by_zipcode_census_2000.zip
 # Remove Header
 awk 'FNR>2' DEC_00_SF3_P077_with_ann.csv > DEC_00_SF3_P077_with_ann_noheader.csv
 sed -i 's/\"//g' DEC_00_SF3_P077_with_ann_noheader.csv
 sed -i 's/\ //g' DEC_00_SF3_P077_with_ann_noheader.csv
-
-clear
 
 # drop any existing tables
 echo 
@@ -25,13 +25,26 @@ echo "drop '$HBASE_TABLE'" >> hb1
 echo "exit" >> hb1
 
 hbase shell hb1
+clear
+
+echo ------------------------------------------------------
+echo Display the structure of the original file
+echo ------------------------------------------------------
+echo
+echo cat DEC_00_SF3_P077_with_ann_noheader.csv | head -5
+echo "(press Enter)"
+read
+cat DEC_00_SF3_P077_with_ann_noheader.csv | head -5
+
 
 echo 
 echo ------------------------------------------------------
-echo  Create the Hive table using HBaseStorageHandler
+echo  "Create the Hive table using HBaseStorageHandler"
 echo ------------------------------------------------------
 echo 
-
+echo "create '$HBASE_TABLE', 'id', 'zip', 'desc', 'income'"
+echo "(press Enter)"
+read
 echo "create '$HBASE_TABLE', 'id', 'zip', 'desc', 'income'" > hb1
 echo "exit" >> hb1
 hbase shell hb1
@@ -42,15 +55,17 @@ echo ------------------------------------------------------
 echo "Create Hive's external table"
 echo ------------------------------------------------------
 echo 
+echo "CREATE EXTERNAL TABLE $HIVE_TABLE (key STRING,zip STRING,desc1 STRING,desc2 STRING,income STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' STORED BY 'org.apache.hadoop.hive.hbase.HBaseStorageHandler' WITH SERDEPROPERTIES (\"hbase.columns.mapping\" = \":key,zip:zip,desc:desc1,desc:desc2,income:income\") TBLPROPERTIES(\"hbase.table.name\" = \"$HBASE_TABLE\");"
+echo "(press Enter)"
+read
 
 hive -e "CREATE EXTERNAL TABLE $HIVE_TABLE (key STRING,zip STRING,desc1 STRING,desc2 STRING,income STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' STORED BY 'org.apache.hadoop.hive.hbase.HBaseStorageHandler' WITH SERDEPROPERTIES (\"hbase.columns.mapping\" = \":key,zip:zip,desc:desc1,desc:desc2,income:income\") TBLPROPERTIES(\"hbase.table.name\" = \"$HBASE_TABLE\");"
 
 echo 
 echo ------------------------------------------------------
-echo Load data into HBase via PIG
+echo "Load data into HBase via PIG"
 echo ------------------------------------------------------
 echo 
-
 echo "copyFromLocal DEC_00_SF3_P077_with_ann_noheader.csv ziptest.csv" > pig1
 echo "A = LOAD 'ziptest.csv' USING PigStorage(',') as (id:chararray, zip:chararray, desc1:chararray, desc2:chararray, income:chararray); STORE A INTO 'hbase://$HBASE_TABLE' USING org.apache.pig.backend.hadoop.hbase.HBaseStorage('zip:zip,desc:desc1,desc:desc2,income:income');" >> pig1
 
@@ -88,6 +103,12 @@ echo
 read
 impala-shell -i $IMPALA_HOST $IMPALA_OPTS -q "select * from $HIVE_TABLE limit 4"
 echo
+
+echo
+echo "=> select count(*) from $HIVE_TABLE (press Enter)"
+echo 
+read
+impala-shell -i $IMPALA_HOST $IMPALA_OPTS -q "select count(*) from $HIVE_TABLE;"
 
 echo
 echo "=> select count(*) from $HIVE_TABLE where income>'0' and income<'7000' (press Enter)"
